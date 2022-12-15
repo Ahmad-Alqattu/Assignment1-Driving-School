2030c1addbba9e7e7cbcec1f4a639b0ef091ab2b
SajedAbdAlkareem <sajedbaqa130@gmail.com>
2022-12-15 19:53:06 +0200

    sajedNew

---
+++ app/src/main/java/edu/cs/birzeit/assignment1_driving_school/Login.java
@@ -0,0 +1,27 @@
+package edu.cs.birzeit.assignment1_driving_school;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    private Button butLog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        butLog = findViewById(R.id.butLog);
+
+    }
+
+    public void subLog(View view) {
+        Intent v= new Intent( Login.this,Home.class);
+        startActivity(v);
+        finish();
+
+    }
+}
--- app/src/main/java/edu/cs/birzeit/assignment1_driving_school/MainActivity.java
+++ app/src/main/java/edu/cs/birzeit/assignment1_driving_school/MainActivity.java
@@ -2,23 +2,43 @@ package edu.cs.birzeit.assignment1_driving_school;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tViewMain;
+    private ImageView imgMain;
+    private Animation bottom ,top;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //sfdads
-///fdghghgdfh
-        //hala
+
+        tViewMain = findViewById(R.id.tViewMain);
+        imgMain = findViewById(R.id.imgMain);
+        top = AnimationUtils.loadAnimation(this,R.anim.topanim);
+        bottom = AnimationUtils.loadAnimation(this,R.anim.bootmanim);
+        imgMain.setAnimation(top);
+        tViewMain.setAnimation(bottom);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent view = new Intent( MainActivity.this,Login.class);
+                startActivity(view);
+                finish();
+            }
+        }, 7000);
 
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+
 }
