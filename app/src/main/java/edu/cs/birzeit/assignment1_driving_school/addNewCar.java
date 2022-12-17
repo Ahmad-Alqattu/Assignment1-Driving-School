5876f321c9e8b1a415a1e404ce66ac845d3e5f41
SajedAbdAlkareem <sajedbaqa130@gmail.com>
2022-12-17 23:49:21 +0200

    sajedNew

--- app/src/main/java/edu/cs/birzeit/assignment1_driving_school/addNewCar.java
+++ app/src/main/java/edu/cs/birzeit/assignment1_driving_school/addNewCar.java
@@ -3,12 +3,38 @@ package edu.cs.birzeit.assignment1_driving_school;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import edu.cs.birzeit.assignment1_driving_school.model.Car;
+import edu.cs.birzeit.assignment1_driving_school.model.CarData;
 
 public class addNewCar extends AppCompatActivity {
-
+    private EditText edit4;
+    private EditText edit3;
+    private EditText edit2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_car);
+        edit2 = findViewById(R.id.edit2);
+        edit3 = findViewById(R.id.edit3);
+        edit4 = findViewById(R.id.edit4);
+    }
+
+    public void btnNewCar(View view) {
+        String edt2 = edit2.getText().toString();
+        String edt3 = edit2.getText().toString();
+        String edt4 = edit2.getText().toString();
+        CarData newData = null;
+        if(!edt2.isEmpty() && !edt3.isEmpty() && !edt4.isEmpty() ){
+            newData.item.add(new Car(edt2,edt3,edt4));
+        }
+        else
+        {
+            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        }
+        
     }
 }
