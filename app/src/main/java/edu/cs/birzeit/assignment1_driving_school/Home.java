0346d0a736469a45c377520be7120944037a887b
ahmad qatow <ahmadl.qatu@gmail.com>
2022-12-17 23:33:57 +0200

    Initial commit

--- app/src/main/java/edu/cs/birzeit/assignment1_driving_school/Home.java
+++ app/src/main/java/edu/cs/birzeit/assignment1_driving_school/Home.java
@@ -17,7 +17,7 @@ int img[]={R.drawable.add_user,R.drawable.drive,R.drawable.statas,R.drawable.car
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         s1=getResources().getStringArray(R.array.cat_btn);
-        category_btn=findViewById(R.id.recyclerView);
+//        category_btn=findViewById(R.id.recyclerView);
         myAdabt myadabt= new myAdabt(this,s1,img);
         category_btn.setAdapter(myadabt);
         category_btn.setLayoutManager(new LinearLayoutManager(this));
